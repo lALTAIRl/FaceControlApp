18fda245290dd158f8590697fae8ac988d6a0e47
Ilja <workgarbuzIlja@gmail.com>
2021-06-10 21:13:36 +0300

    views updated

--- FaceControlApp.Web/Startup.cs
+++ FaceControlApp.Web/Startup.cs
@@ -13,7 +13,7 @@
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
